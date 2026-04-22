1f84e3fede1957ec62bc963a6564315a25d138e4
Aybike Celebi Visser <celebiaybike@gmail.com>
2026-04-22 09:21:57 +0200

    some changes

---
+++ To-Do-App/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <>
+    <div>
+    <nav className="navbar">
+      <a href="/" className="navbar-logo">
+        <div className="navbar-logo-icon">✅</div>
+        <div className="navbar-logo-text">
+          <span className="navbar-logo-name">To Do To Check</span>
+          <br />
+          <span className="navbar-logo-tagline">
+            Professionally or Socially, the Only Task Manager You Need
+          </span>
+        </div>
+      </a>
+    </nav>
+    </div>
+    </>
+  );
+}
+export default Navbar;

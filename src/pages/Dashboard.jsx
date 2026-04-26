690c8f3feb8d422d4c74d636e112989d81e1c074
chukwudiokolomansion <okolomansion@gmx.de>
2026-04-26 08:23:32 +0200

    somestyles

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,19 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import tasks from "./task.json";
+import "../App.css";
+
+const colors = [
+  "#ff6b6b",
+  "#cdeac0",
+  "#ff8787",
+  "#4ecdc4",
+  "#48bfe3",
+  "#cdb4db",
+  "#ffb347",
+  "#bdb2ff",
+  "#ffd166",
+  "#ffafcc",
+  "#90dbf4",
+  "#ff8c61",
+];
 
 function Dashboard() {
   return (
-    <div>
-      <h2>Dashboard</h2>
+    <div className="container">
+      <div className="grid">
+        {/*<h2>Dashboard</h2>*/}
 
-      {tasks.map((item, index) => (
-        <div key={index}>
-          <Link to={`/item/${index}`}>
-            {item.task}
-          </Link>
-        </div>
-      ))}
+        {tasks.map((item, index) => (
+          <div 
+            key={index}
+            className="card"
+            style={{
+              backgroundColor: colors[index % colors.length],
+              transform: `rotate(${(index % 2 === 0 ? -3 : 3)}deg)`,
+            }}
+          >  
+          <Link to={`/item/${index}`}>{item.task}</Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
